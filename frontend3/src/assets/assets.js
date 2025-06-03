d0150758dc435e14a6a0723171891bbea597b9d6
Harsh-HK7 <harshkalal746@gmail.com>
2025-06-03 15:45:52 +0530

    Update assets.js

--- frontend3/src/assets/assets.js
+++ frontend3/src/assets/assets.js
@@ -1,5 +1,5 @@
-import uploaad_area from './upload-image.png';
-import uploaad_song from './upload-music.png';
+import upload_area from './upload-image.PNG';
+import upload_song from './upload-music.PNG';
 import upload_added from './upload-added.png';
 import logo from './logo.png';
 import logo4 from './logo4.png';
@@ -8,8 +8,8 @@ import banner from './banner_cate.png';
 
 
 export const assets = {
-    upload_area: uploaad_area,
-    upload_song: uploaad_song,
+    upload_area: upload_area,
+    upload_song: upload_song,
     upload_added: upload_added,
     logo: logo,
     logo4: logo4,

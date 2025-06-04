90bf412af81bad35ca894ae8e71f4a72d97b25bc
Harsh-HK7 <harshkalal746@gmail.com>
2025-06-04 11:31:00 +0530

    Delete Backend directory

--- Backend/config/mongoDB.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-const connectDB = async () => {
-    mongoose.connection.on("connected", () =>console.log("Databse connected"))
-    await mongoose.connect(`${process.env.MONGO_URI}/music`)
-
-}
-export default connectDB
